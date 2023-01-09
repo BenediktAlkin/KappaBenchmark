6094e3bcf91d0abdd25658614970309395bd0b10
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-10 00:32:07 +0100

    Update __init__.py

--- kappabenchmark/__init__.py
+++ kappabenchmark/__init__.py
@@ -1,7 +1,3 @@
-import benchmarks
-import dataloading
-import run
-
 from .benchmarks import BENCHMARKS
 from .dataloading import benchmark_dataloading, BenchmarkDataloaderResult
 from .run import run_benchmark_grid
